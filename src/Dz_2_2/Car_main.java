5ef0ad568db66f76637c95367b80c0363749710d
MitasovArthur <mitasovartur2@gmail.com>
2021-11-22 16:58:33 +0200

    add Dz_2_2 add Dz_2_3

---
+++ src/Dz_2_2/Car_main.java
@@ -0,0 +1,10 @@
+package Dz_2_2;
+
+public class Car_main {
+    public static void main(String[] args) {
+        Dz_2_2.Car car1 = new Dz_2_2.Car(45);
+        Dz_2_2.Car car2 = new Dz_2_2.Car(64,"Black");
+        System.out.println(car1.year);
+        System.out.println(car2.year +"\t"+ car2.color);
+    }
+}
